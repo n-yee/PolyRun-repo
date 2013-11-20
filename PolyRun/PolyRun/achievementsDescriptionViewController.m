19e5608974faf16c435b9389056e5613d1b92ee2
Brendan Baronia <brendanbaronia@pcp069561pcs.wireless.calpoly.edu>
2013-11-19 16:14:27 -0800

    Finished basic coding for title and description for each achievement

--- PolyRun/PolyRun/achievementsDescriptionViewController.m
+++ PolyRun/PolyRun/achievementsDescriptionViewController.m
@@ -11,12 +11,12 @@
 @interface AchievementsDescriptionViewController ()
 @property (weak, nonatomic) IBOutlet UINavigationItem *navigationBar;
 @property (weak, nonatomic) IBOutlet UIImageView *achievementPicture;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionBox;
 
 @end
 
 @implementation AchievementsDescriptionViewController
 NSString * achievement;
-NSDictionary * titleLabel;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -30,15 +30,19 @@ NSDictionary * titleLabel;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    NSBundle * mainBundle = [NSBundle mainBundle];
+    NSMutableDictionary * titleLabel = [NSMutableDictionary dictionary];
     NSUserDefaults * defaults = [NSUserDefaults standardUserDefaults];
-    [titleLabel setValue: @"Total Distance Run" forKey:@"totalDistanc"];
-    [titleLabel setValue: @"Total Pins Reached" forKey: @"numberOfPins"];
-    [titleLabel setValue:@"Regular Visitor" forKey:@"keepVisiting"];
-    [titleLabel setValue:@"Total Number Of Runs" forKey:@"numberOfRuns"];
+    [titleLabel setObject: @"Total Distance Run" forKey:@"totalDistanc"];
+    [titleLabel setObject: @"Total Pins Reached" forKey: @"numberOfPins"];
+    [titleLabel setObject: @"Regular Visitor" forKey:@"keepVisiting"];
+    [titleLabel setObject: @"Total Number Of Runs" forKey:@"numberOfRuns"];
     achievement = [defaults objectForKey:@"achievementValue"];
-    _navigationBar.title = [titleLabel objectForKey: [achievement substringToIndex:12]];
+    NSString * twelveLetterAchievement = [achievement substringToIndex:12];
+    _navigationBar.title = [titleLabel objectForKey: twelveLetterAchievement];
     if ([defaults boolForKey:[NSString stringWithFormat:@"%@Status", [defaults objectForKey:@"achievementValue"]]]) _achievementPicture.image = [UIImage imageNamed:[NSString stringWithFormat:@"%@Success.png", achievement]];
     else _achievementPicture.image = [UIImage imageNamed:[NSString stringWithFormat:@"%@Fail.png", [achievement substringToIndex:12]]];
+    _descriptionBox.text = [NSString stringWithContentsOfFile:[mainBundle pathForResource:twelveLetterAchievement ofType:@"txt"] encoding:NSUTF8StringEncoding error:nil];
 	// Do any additional setup after loading the view.
 }
 
