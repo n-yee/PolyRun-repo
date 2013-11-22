51a7627c62184573ac2cc88120ae8393984bfdbd
Rishab Hegde <ruhegde@232x19.csc.calpoly.edu>
2013-11-21 16:27:32 -0800

    Hopefully this works

---
+++ PolyRun/Checkpoint.h
@@ -0,0 +1,15 @@
+//
+//  Checkpoint.h
+//  PolyRun
+//
+//  Created by Rishab Hegde on 11/21/13.
+//  Copyright (c) 2013 Nathan Yee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface Checkpoint : NSObject
+@property CLLocation *loc;
+@property double distance;
+@end
---
+++ PolyRun/Checkpoint.m
@@ -0,0 +1,13 @@
+//
+//  Checkpoint.m
+//  PolyRun
+//
+//  Created by Rishab Hegde on 11/21/13.
+//  Copyright (c) 2013 Nathan Yee. All rights reserved.
+//
+
+#import "Checkpoint.h"
+
+@implementation Checkpoint
+
+@end
